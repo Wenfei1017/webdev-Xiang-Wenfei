2b3b9b33457902a8a4bf50b08c7c1396a139f10c
xwenfei <xiang.we@husky.neu.edu>
2018-01-15 16:03:04 -0800

    update app.js

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -13,7 +13,7 @@ module.exports = function(app)
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // use yours
+    connectionString += 'mongodb://MLAB_USERNAME_WEBDEV:MLAB_PASSWORD_WEBDEV@ds157097.mlab.com:57097/heroku_wq4lb13n'; // use yours
   }
 
   var mongoose = require("mongoose");
