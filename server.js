1ed4880cd5531fed10f464b6c424e1060b0a4ce7
xwenfei <xiang.we@husky.neu.edu>
2018-02-21 21:31:49 -0800

    modify server.js file

--- server.js
+++ server.js
@@ -1,5 +1,53 @@
-
-// Get the dependencies
+//
+// // Get the dependencies
+//
+// const express = require('express');
+// const path = require('path');
+// const http = require('http');
+// const bodyParser = require('body-parser');
+// const app = express();
+//
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+//
+//
+//
+//
+// // Point static path to dist -- For building -- REMOVE
+// app.use(express.static(path.join(__dirname, 'dist')));
+//
+//
+//
+// // CORS
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
+//
+//
+//
+//
+// const port = process.env.PORT || '5000';
+// app.set('port', port);
+//
+//
+// // Create HTTP server
+// const server = http.createServer(app);
+//
+// server.listen( port );
+// const server = http.createServer(app);
+//
+// /*
+// // For Build: Catch all other routes and return the index file -- BUILDING
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
+// */
+//
+//
+// server.listen( port , () => console.log('Running on port 5000'));
 
 const express = require('express');
 const path = require('path');
@@ -10,13 +58,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
-
-
 
 // CORS
 app.use(function(req, res, next) {
@@ -26,25 +69,18 @@ app.use(function(req, res, next) {
   next();
 });
 
-
-
-
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '3100';
 app.set('port', port);
-
 
 // Create HTTP server
 const server = http.createServer(app);
 
-server.listen( port );
-const server = http.createServer(app);
+//var serverSide = require("./server/test-mongodb/app");
+//serverSide(app);
 
-/*
 // For Build: Catch all other routes and return the index file -- BUILDING
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-*/
 
-
-server.listen( port , () => console.log('Running on port 5000'));
+server.listen( port , () => console.log('Running'));
