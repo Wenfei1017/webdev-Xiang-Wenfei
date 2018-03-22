cf8a7da486010d41d42d9aa1a7198d2c0069b59b
xwenfei <xiang.we@husky.neu.edu>
2018-03-21 20:41:59 -0700

    update

--- assignment/services/widget.service.server.js
+++ assignment/services/widget.service.server.js
@@ -45,7 +45,7 @@ module.exports = function (app) {
     var pageId = req.body.pageId;
 
     if(myFile == null) {
-      res.redirect(baseUrl + "/user/" +userId + "/website/" + websiteId + "/page/" + pageId + "/widget");
+      res.redirect(baseUrl + "/user/" +userId + "/website/" + websiteId + "/page/" + pageId + "/widget/" + widgetId);
       // res.redirect("https://cs5610-webdev-wenfei.herokuapp.com/profile/" + userId + "/website/" + websiteId + "/page/" + pageId + "/widget");
       return;
     }
@@ -75,7 +75,7 @@ module.exports = function (app) {
     }
 
     // res.redirect("http://localhost:4200/profile/" + userId + "/website/" + websiteId + "/page/" + pageId + "/widget");
-    res.redirect("https://cs5610-webdev-wenfei.herokuapp.com/user/" + userId + "/website/" + websiteId + "/page/" + pageId + "/widget");
+    res.redirect("https://cs5610-webdev-wenfei.herokuapp.com/user/" + userId + "/website/" + websiteId + "/page/" + pageId + "/widget/" + widgetId);
     // res.redirect(baseUrl + "/user/" + userId + "/website/" + websiteId + "/page/" + pageId + "/widget");
   }
 
