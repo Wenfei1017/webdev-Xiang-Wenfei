c7c70b0bd5384f087db355d21566267deb002d08
xwenfei <xiang.we@husky.neu.edu>
2018-03-30 17:31:39 -0700

    update 1

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 // var db = mongoose.connect('mongodb://localhost:27017/webdev');
-var db = mongoose.connect('mongodb://wenfei1:123456@ds127139.mlab.com:57097/heroku_wq4lb13n');
+var db = mongoose.connect('mongodb://wenfei1:123456@ds157097.mlab.com:57097/heroku_wq4lb13n');
 
 module.exports = db;
